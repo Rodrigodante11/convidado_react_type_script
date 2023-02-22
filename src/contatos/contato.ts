368503bd0e488b0f4ae10cb152696ac2343fcb3e
Rodrigodante11 <rodrigoaugusto839@gmail.com>
2023-02-22 13:08:48 -0300

    utilizando formik

--- src/contatos/contato.ts
+++ src/contatos/contato.ts
@@ -1,5 +1,5 @@
 export interface Contato {
     id?:string;  // ? == opcional
     nome:string;
-    idade:string;
+    idade:number | '';
 }
--- src/contatos/form.tsx
+++ src/contatos/form.tsx
@@ -4,31 +4,53 @@ import { TextField } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid'
 
+import { useFormik, FormikHelpers } from 'formik';
 import { Contato } from './contato'
+
+import './index.css';
 
 interface ContatosFormProps {
     contato?: Contato;
 }
 
 export const ContatosForm: React.FC<ContatosFormProps> = ({}: ContatosFormProps) =>{
+
+    const formik = useFormik<Contato>({
+        onSubmit: (c: Contato) => console.log(c),
+        initialValues: { nome: '', idade: '' } // valor inicial
+    })
+
     return (
-        <form>
+        <form onSubmit={formik.handleSubmit}>
             <Grid container={true}>
 
                 <Grid item xs={12}>
-                    <TextField variant='outlined' label="Nome">
+                    <TextField variant='outlined' 
+                                label="Nome"
+                                id="inputNome"
+                                name="nome"
+                                value={formik.values.nome}
+                                onChange={formik.handleChange}
+                                className="full-width">
 
                     </TextField>
                 </Grid>
 
                 <Grid item xs={12}>
-                    <TextField variant='outlined' label="Idade">
+                    <TextField  variant='outlined' 
+                                label="Idade"
+                                name="idade"
+                                id="inputIdade"
+                                value={formik.values.idade}
+                                onChange={formik.handleChange}
+                                className="full-width">
+
 
                     </TextField>
                 </Grid>
 
                 <Grid item xs={12}>
-                    <Button variant="contained" color="primary">
+                    <Button type="submit" variant="contained" color="primary"className="full-width">
                         Adicionar
                     </Button>
                 </Grid>
